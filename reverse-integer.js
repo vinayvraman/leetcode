72073df41f75cee9ae10159f0fc29fd3f32ed4f2
vinayvraman <vinayvraman@users.noreply.github.com>
2025-11-18 00:30:33 +0530

    Create reverse-integer.js

---
+++ reverse-integer.js
@@ -0,0 +1,45 @@
+/*
+7. Reverse Integer
+Solved
+Medium
+Topics
+premium lock icon
+Companies
+Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
+
+Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
+
+ 
+
+Example 1:
+
+Input: x = 123
+Output: 321
+Example 2:
+
+Input: x = -123
+Output: -321
+Example 3:
+
+Input: x = 120
+Output: 21
+ 
+
+Constraints:
+
+-231 <= x <= 231 - 1
+*/
+
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var reverse = function(x) {
+    let xRev = parseInt(x.toString().split('').reverse().join(''))
+    if(x < 0)
+        xRev = xRev * -1
+    
+    if (xRev > Math.pow(-2, 31) && xRev < (Math.pow(2, 31) - 1))
+        return xRev
+    return 0
+};
